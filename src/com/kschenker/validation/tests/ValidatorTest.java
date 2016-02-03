8d89c5a57409c5a6a84a5989065aba66b05f32ce
Kevin Schenker <schenker.kevin@gmail.com>
2016-02-03 14:07:39 +0100

    Implemented some test methods for the Validator class.

--- src/com/kschenker/validation/tests/ValidatorTest.java
+++ src/com/kschenker/validation/tests/ValidatorTest.java
@@ -1,10 +1,10 @@
 package com.kschenker.validation.tests;
 
+import com.kschenker.validation.Validator;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 /**
@@ -33,16 +33,44 @@ public class ValidatorTest
     }
 
     @Test
-    public void isNullOrEmpty1_FeatureToTest() throws Exception
+    public void isNullOrEmpty_ReturnTrueIfStringIsNull() throws Exception
     {
-        assertThat("Not implemented yet", false);
+        String aString = null;
 
+        assertThat(Validator.isNullOrEmpty(aString), is(true));
     }
 
     @Test
-    public void isSameObject_FeatureToTest() throws Exception
+    public void isNullOrEmpty_ReturnTrueIfStringIsEmpty() throws Exception
     {
-        assertThat("Not implemented yet", false);
+        String aString = "";
 
+        assertThat(Validator.isNullOrEmpty(aString), is(true));
+    }
+
+    @Test
+    public void isNullOrEmpty_ReturnFalseIfStringHasAtLeastOneCharacter() throws Exception
+    {
+        String aString = "x";
+
+        assertThat(Validator.isNullOrEmpty(aString), is(false));
+    }
+
+    @Test
+    public void isSameObject_ReturnFalseIfEitherArgumentIsNull() throws Exception
+    {
+        assertThat(Validator.isSameObject(null, ""), is(false));
+        assertThat(Validator.isSameObject(null, null), is(false));
+        assertThat(Validator.isSameObject("", null), is(false));
+    }
+
+    @Test
+    public void isSameObject_ReturnTrueIfArgumentsReferenceSameObject() throws Exception
+    {
+        String first = "";
+        String second = first;
+
+        assertThat(Validator.isSameObject(first, second), is(true));
+        assertThat(Validator.isSameObject(second, first), is(true));
     }
 }
