cd391bb09d95e230d8e28e0d678cb38ef0198950
Kevin Schenker <schenker.kevin@gmail.com>
2016-02-03 13:24:56 +0100

    Added unit test skeleton for validation package.

---
+++ src/com/kschenker/validation/tests/ValidatorTest.java
@@ -0,0 +1,48 @@
+package com.kschenker.validation.tests;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+
+
+/**
+ * Author: Kevin Schenker
+ * Date: 03.02.2016
+ * <p>
+ * Project: Utilities
+ * Package: com.kschenker.validation.tests
+ */
+public class ValidatorTest
+{
+
+    @Test
+    public void testIsNullOrEmpty() throws Exception
+    {
+        assertThat("Not implemented yet", false);
+
+    }
+
+
+    @Test
+    public void isNullOrEmpty_FeatureToTest() throws Exception
+    {
+        assertThat("Not implemented yet", false);
+
+    }
+
+    @Test
+    public void isNullOrEmpty1_FeatureToTest() throws Exception
+    {
+        assertThat("Not implemented yet", false);
+
+    }
+
+    @Test
+    public void isSameObject_FeatureToTest() throws Exception
+    {
+        assertThat("Not implemented yet", false);
+
+    }
+}
